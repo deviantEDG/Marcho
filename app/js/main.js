2cc9c558d8daa8ff1adb3636fc2c2ff1cab8c614
deviantEDG <shura.bobrov.96@mail.ru>
2021-10-31 12:21:37 +0500

    added adaptive project

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(function () {
 		$('.menu__list').toggleClass('menu__list--active');
 	});
 
+	$('.shop__filter-btn').on('click', function(){
+		$('.shop__filters').slideToggle();
+	});
+
 	$('.footer-top__title').on('click', function () {
 		$(this).siblings().slideToggle();
 		$(this).toggleClass('active');
@@ -40,6 +44,14 @@ $(function () {
 		asNavFor: '.product-slide__thumb',
 		arrows: false,
 		fade: true,
+		responsive: [
+			{
+				breakpoint: 1051,
+				settings: {
+					draggable: true
+				}
+			},
+		]
 	});
 
 	$('.shop-content__filter-btn').on('click', function () {
@@ -49,10 +61,12 @@ $(function () {
 
 	$('.button-list').on('click', function () {
 		$('.product-item').addClass('product-item--list');
+		$('.shop-content__inner').addClass('shop-content__nogrid');
 	});
 
 	$('.button-grid').on('click', function () {
 		$('.product-item').removeClass('product-item--list');
+		$('.shop-content__inner').removeClass('shop-content__nogrid');
 	});
 
 
